18cada202393bffb4ddcd5bdfb0d8e0852f9b84f
mmersic <mmersic@gmail.com>
2024-12-09 21:11:21 -0500

    Day 7 - One solve method.

--- src/main/java/org/mersic/Day07.java
+++ src/main/java/org/mersic/Day07.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Day07 {
     
-    public static long partOneSolve(long lhs, long total, long[] rhs, int ri) {
+    public static long solve(long lhs, long total, long[] rhs, int ri, int ops) {
         if (lhs == total && rhs.length == ri) {
             return total;
         } else if (rhs.length == ri) {
@@ -15,40 +15,24 @@ public class Day07 {
         } else if (total > lhs) {
             return 0;
         } else {
-            long result = partOneSolve(lhs, total + rhs[ri], rhs, ri+1);
+            long result = solve(lhs, total + rhs[ri], rhs, ri+1, ops);
             if (result == lhs) {
                 return lhs;
-            } else {
-                return partOneSolve(lhs, total * rhs[ri], rhs, ri+1);
             }
-        }
-    }
-
-    public static long partTwoSolve(long lhs, long total, long[] rhs, int ri) {
-        if (lhs == total && rhs.length == ri) {
-            return total;
-        } else if (rhs.length == ri) {
-            return 0;
-        } else if (total > lhs) {
-            return 0;
-        } else {
-            long result = partTwoSolve(lhs, total + rhs[ri], rhs, ri+1);
+            result = solve(lhs, total * rhs[ri], rhs, ri+1, ops);
             if (result == lhs) {
                 return lhs;
+            }
+            if (ops == 3) {
+                return solve(lhs, Long.parseLong(total + "" + rhs[ri]), rhs, ri + 1, ops);
             } else {
-                result = partTwoSolve(lhs, total * rhs[ri], rhs, ri+1);
-                if (result == lhs) {
-                    return lhs;
-                } else {
-                    return partTwoSolve(lhs, Long.parseLong(total + "" + rhs[ri]), rhs, ri+1);
-                }
+                return 0;
             }
         }
     }
     
     public static void main(String[] args) throws Exception{
         List<String> input = Files.readAllLines(Path.of(Day07.class.getClassLoader().getResource("day.07.input").toURI()));
-        
         long start = System.currentTimeMillis();
         long partOne = 0;
         long partTwo = 0;
@@ -56,8 +40,8 @@ public class Day07 {
             String[] S = line.split(": ");
             long lhs = Long.parseLong(S[0]);
             long[] rhs = Arrays.stream(S[1].split(" ")).mapToLong(Long::parseLong).toArray();
-            partOne += partOneSolve(lhs, rhs[0], rhs, 1);
-            partTwo += partTwoSolve(lhs, rhs[0], rhs, 1);
+            partOne += solve(lhs, rhs[0], rhs, 1, 2);
+            partTwo += solve(lhs, rhs[0], rhs, 1, 3);
         }
         long end = System.currentTimeMillis();
         
